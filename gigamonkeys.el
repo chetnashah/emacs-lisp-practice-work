c781ee787a1b70b63633a68f19febb5c92467670
chetnashah <chetshah80@gmail.com>
2016-10-28 17:29:37 +0530

    add gigamonkeys code

---
+++ gigamonkeys.el
@@ -0,0 +1,43 @@
+
+
+;; starts from http://www.gigamonkeys.com/book/practical-a-simple-database.html
+
+;; normal lisp list
+(list 1 2 3)
+
+;; another flavor of list is property-list or plist
+;; list item occur as :symbol value pair
+;; keywords are used as names of pair (coz they start with :)
+
+(list :a 1 :b 2 :c 3)
+
+;; the advantage of plists comes by using a function known as "getf"
+;; getf takes a list and a keyword and returns value for keyword
+
+;; getf usage with plists
+(getf (list :a 1 :b 2) :a) ;; 1
+(getf (list :a 1 :b 2) :b) ;; 2
+
+;;; plists can be useful for making structs or record objects
+;; e.g. make-cd
+
+(defun make-cd (title artist rating ripped)
+  (list :title title :artist artist :rating rating :ripped ripped))
+
+(make-cd "Roses" "kathy" 3 t)  ;; make a cd record
+
+(getf (make-cd "Roses" "kathy" 3 t) :rating) ;; 3 , get rating of the made cd record
+
+
+
+
+;; to make a data base we need more than a record, we need a list of records
+;; or in our case list of lists
+
+;; convention global variables have asteriks, we have a global database variable
+(defvar *db* nil)
+
+;; we will implement CRUD 
+
+
+
