537528dc3a42a193eea53470de9ef7b7b2435ebc
chetnashah <chetshah80@gmail.com>
2017-01-31 12:10:18 +0530

    add emacs specific elisp functions file

---
+++ emacspecificfuncs.el
@@ -0,0 +1,17 @@
+
+
+;; some completion at point functions
+;; try-completion used for completions from a list of possibel strings
+
+(try-completion "b" (list "foo" "baazinga" "badname" "foobaz")) ;; ba
+
+
+;;region and buffer related functions
+
+
+
+
+;; hooks, eval load related functions
+
+
+;; package, require etc. related functions
